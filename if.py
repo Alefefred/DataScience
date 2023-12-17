5f6b2977ee72cb46e01b95f4fe19c27b432e0f19
Alefefred <98221006+Alefefred@users.noreply.github.com>
2023-12-17 17:33:32 -0300

    Exercício de teste sobre estrutura condicional

---
+++ if.py
@@ -0,0 +1,9 @@
+#Um breve teste da estrutura if else no python
+
+aviao = 600
+carroCorrida = 350
+
+if aviao > carroCorrida:
+    print ("O avião é mais rápido!")
+else:
+    print ("O carro é mais rápido!")
